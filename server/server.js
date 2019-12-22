a123e528c5513b80457064344758869263385fd2
vikramnr <doyouthinkyouknowha@gmail.com>
2019-12-22 08:52:12 +0530

    added create and new message events

--- server/server.js
+++ server/server.js
@@ -11,14 +11,24 @@ const PORT = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
-io.on('connection', (_socket) => {
-    // console.log(socket);
-    console.log('new user connect');
+io.on('connection', (socket) => {
+    socket.emit('newMessage', {
+        from: `server`,
+        text: 'hello der'
+    })
+    socket.on('createMessage', (message) => {
+        socket.emit('newMessage',{
+            from: message.from,
+            text: message.text
+        })
+    })
 });
 
 io.on('disconnect', (_socket) => {
     console.log('disconneted from server');
 });
+
+
 
 server.listen(PORT, () => {
     console.log(`server started in Port ${PORT}`);
