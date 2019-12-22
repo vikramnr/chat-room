4e4a7f2815712260f361564872fe4cd0ebed228d
vikramnr <doyouthinkyouknowha@gmail.com>
2019-12-22 09:38:38 +0530

    added file change

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+socket.on('connect', () => {
+    console.log('user connected');
+    socket.on('newMessage', (message) => {
+        console.log(message);
+        document.getElementById('text').textContent = message.text + ' from ' + message.from;
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('disconneted from server');
+})
+const send = document.getElementById('send');
+send.addEventListener('click', (e) => {
+    socket.emit('createMessage', {
+        from: 'Andrew',
+        text: document.getElementById('message').value
+    });
+});
